86088877dba4996a24748f5434864711bfb3a945
Lucas Melo <lucas.melo044@gmail.com>
2024-12-29 19:19:47 -0300

    add favicon

--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -24,6 +24,7 @@ class SiteSetup(models.Model):
     class Meta:
         verbose_name = 'Setup'
         verbose_name_plural = 'Setup'
+
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=255)
 
@@ -33,6 +34,11 @@ class SiteSetup(models.Model):
     show_description = models.BooleanField(default=True)
     show_pagination = models.BooleanField(default=True)
     show_footer = models.BooleanField(default=True)
+
+    favicon = models.ImageField(
+        upload_to='assetes/favicon/%Y/%m',
+        blank=True, default=''
+    )
 
     def __str__(self):
         return self.title
