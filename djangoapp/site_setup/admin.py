3427c87649760de86891a7b0c4e0b49addb521ff
Lucas Melo <lucas.melo044@gmail.com>
2024-12-29 17:33:18 -0300

    create MenuLink model

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -1,3 +1,11 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from site_setup.models import MenuLink
+
+
+@admin.register(MenuLink)
+class MenuLinkAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', 'url_or_path', 'new_tab')
+    list_display_links = ('id', 'text', 'url_or_path')
+    search_fields = ('id', 'text', 'url_or_path')
