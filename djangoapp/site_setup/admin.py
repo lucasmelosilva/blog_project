6829de74d8b5c520d47382e62122f64df2702671
Lucas Melo <lucas.melo044@gmail.com>
2024-12-29 17:41:31 -0300

    create SiteSetup model

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -1,11 +1,19 @@
 
 from django.contrib import admin
 
-from site_setup.models import MenuLink
+from site_setup.models import MenuLink, SiteSetup
 
 
 @admin.register(MenuLink)
 class MenuLinkAdmin(admin.ModelAdmin):
-    list_display = ('id', 'text', 'url_or_path', 'new_tab')
-    list_display_links = ('id', 'text', 'url_or_path')
-    search_fields = ('id', 'text', 'url_or_path')
+    list_display = 'id', 'text', 'url_or_path', 'new_tab',
+    list_display_links = 'id', 'text', 'url_or_path',
+    search_fields = 'id', 'text', 'url_or_path',
+
+
+@admin.register(SiteSetup)
+class SiteSetupAdmin(admin.ModelAdmin):
+    list_display = 'title', 'description',
+
+    def has_add_permission(self, request):
+        return not SiteSetup.objects.exists()
