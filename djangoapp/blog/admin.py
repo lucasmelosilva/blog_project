757b4a3362af06bb1a278b39c2e1d96b09520dcb
Lucas Melo <lucas.melo044@gmail.com>
2025-01-05 20:48:58 -0300

    add summernote

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from blog.models import Page, Post, Tag, Category
+from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Tag)
@@ -41,7 +42,8 @@ class PageAdmin(admin.ModelAdmin):
 
 
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = 'content',
     list_display = 'id', 'title', 'is_published',  # 'created_by',
     list_display_links = 'title',
     search_fields = 'id', 'slug', 'title', 'excerpt', 'content',
