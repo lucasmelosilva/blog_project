62817ada814509bbcd2b1a74587f9000bb6b5982
Lucas Melo <lucas.melo044@gmail.com>
2025-01-09 18:13:07 -0300

    search form

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.db.models import Q
 from blog.models import Post
 
 PER_PAGE = 9
@@ -74,6 +75,28 @@ def tag(request, slug):
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+        }
+    )
+
+
+def search(request):
+    search_value = request.GET.get('search', '').strip()
+
+    posts = (
+        Post.objects.get_published()  # type: ignore
+        .filter(
+            Q(title__icontains=search_value) |
+            Q(excerpt__icontains=search_value) |
+            Q(content__icontains=search_value)
+        )[:PER_PAGE]
+    )
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': posts,
+            'search_value': search_value,
         }
     )
 
